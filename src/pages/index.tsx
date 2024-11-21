b50271c6e1e3474bbeefc4c6c24222ce41e54683
Kauã Felipe <kauaf@xgrow.com>
2024-11-21 05:39:27 -0300

    feat: add about to in landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import DefaultLayout from "@/layouts/default";
+import { HomepageAboutTo } from "@/layouts/pages/home_page/about_to";
 import { HomepageIntroduction } from "@/layouts/pages/home_page/Introduction";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
       <HomepageIntroduction />
+      <HomepageAboutTo />
     </DefaultLayout>
   );
 }
